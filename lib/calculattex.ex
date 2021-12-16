6b730afd1fe0558075dec7ad42775bad0793290c
pyzlnar@Megumin <pyzlnar@gmail.com>
2021-12-16 17:48:07 -0600

    :tada: Initial Commit

---
+++ lib/calculattex.ex
@@ -0,0 +1,18 @@
+defmodule Calculattex do
+  @moduledoc """
+  Documentation for `Calculattex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calculattex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/calculattex_test.exs
@@ -0,0 +1,8 @@
+defmodule CalculattexTest do
+  use ExUnit.Case
+  doctest Calculattex
+
+  test "greets the world" do
+    assert Calculattex.hello() == :world
+  end
+end
